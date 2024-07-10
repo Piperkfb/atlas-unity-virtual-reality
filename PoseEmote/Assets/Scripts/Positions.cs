c366077341d6a60a8620211b0a09a429537ca635
Piperkfb <piperkfb@gmail.com>
2024-07-10 14:56:00 -0500

    little add

---
+++ PoseEmote/Assets/Scripts/Positions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Positions : MonoBehaviour
+{
+    bool leftx = false;
+    bool rightx = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find hand tracking for movement
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
